3f1aabad3f366357c4bf51b46c0be2d284f726da
Олег Сазонов <thexhalferx@icloud.com>
2022-10-26 03:42:43 +0300

    Initial Commit

---
+++ macOS ToolBox/macOS_ToolBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_ToolBoxApp.swift
+//  macOS ToolBox
+//
+//  Created by Олег Сазонов on 26.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct macOS_ToolBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
