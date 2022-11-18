2095b5fe565a4dc11b131bfd3c7ce04b3d87e026
Олег Сазонов <thexhalferx@icloud.com>
2022-11-18 14:01:26 +0300

    restyled tabView for camper view

--- macOS ToolBox/Views/Functional Views/Camper.swift
+++ macOS ToolBox/Views/Functional Views/Camper.swift
@@ -78,32 +78,37 @@ struct CamperView: View {
     }
     
     private func mainTabView() -> some View {
-        HStack(alignment: .center) {
-            TabView {
-                if isBC {
-                    BootCampView().tabItem {
-                        Text("bootcamp.button")
-                    }
-                }
-                if virtsExist {
-                    Virtuals.FileSearch().tabItem {
-                        Text("VMs")
-                    }
-                }
-            }
-        }
+//        HStack(alignment: .center) {
+//            TabView {
+//                if isBC {
+//                    BootCampView().tabItem {
+//                        Text("bootcamp.button")
+//                    }
+//                }
+//                if virtsExist {
+//                    Virtuals.FileSearch().tabItem {
+//                        Text("VMs")
+//                    }
+//                }
+//            }.tabViewStyle(.automatic)
+//        }
+        CustomTabView(tabBarPosition: .top, content:
+            isBC && virtsExist ? [(tabText: StringLocalizer("bootcamp.button"), tabIconName: "window.ceiling", view: AnyView(BootCampView())),
+            (tabText: StringLocalizer("VMs"), tabIconName: "text.and.command.macwindow", view: AnyView(Virtuals.FileSearch()))] :
+                isBC && !virtsExist ? [(tabText: StringLocalizer("bootcamp.button"), tabIconName: "window.ceiling", view: AnyView(BootCampView()))] :
+                !isBC && virtsExist ? [(tabText: StringLocalizer("VMs"), tabIconName: "text.and.command.macwindow", view: AnyView(Virtuals.FileSearch()))] :
+                [(tabText: StringLocalizer("VMs"), tabIconName: "text.and.command.macwindow", view: AnyView(Virtuals.FileSearch()))]
+        )
     }
     
-    private func MainViewWithoutPassword() -> AnyView {
-        return AnyView(
-            VStack{
-                if isBC || virtsExist {
-                    mainTabView().padding(.all)
-                } else {
-                    nothingFound().padding(.all)
-                }
+    private func MainViewWithoutPassword() -> some View {
+        return VStack{
+            if isBC || virtsExist {
+                mainTabView().padding(.all)
+            } else {
+                nothingFound().padding(.all)
             }
-        )
+        }
     }
     
     var body: some View {
@@ -143,5 +148,82 @@ struct CamperView: View {
 struct camperPreview: PreviewProvider {
     static var previews: some View {
         CamperView()
+    }
+}
+
+private struct CustomTabView: View {
+    
+    public enum TabBarPosition { // Where the tab bar will be located within the view
+        case top
+        case bottom
+    }
+    
+    private let tabBarPosition: TabBarPosition
+    private let tabText: [String]
+    private let tabIconNames: [String]
+    private let tabViews: [AnyView]
+    
+    @State private var selection = 0
+    
+    public init(tabBarPosition: TabBarPosition, content: [(tabText: String, tabIconName: String, view: AnyView)]) {
+        self.tabBarPosition = tabBarPosition
+        self.tabText = content.map{ $0.tabText }
+        self.tabIconNames = content.map{ $0.tabIconName }
+        self.tabViews = content.map{ $0.view }
+    }
+    
+    public var tabBar: some View {
+        
+        HStack {
+            Spacer()
+            ForEach(0..<tabText.count, id: \.self) { index in
+                HStack {
+                    Image(systemName: self.tabIconNames[index])
+                    if self.selection == index {
+                        Text(self.tabText[index])
+                    }
+                }
+                .padding(.all)
+                .foregroundColor(self.selection == index ? Color.primary : Color.secondary)
+                .background(content: {
+                    RoundedRectangle(cornerRadius: 15)
+                        .foregroundStyle(.ultraThinMaterial)
+                })
+                .onTapGesture {
+                    self.selection = index
+                }
+                .animation(SettingsMonitor.secondaryAnimation, value: selection)
+            }
+            Spacer()
+        }
+        .padding(.all)
+        .backgroundStyle(.clear)
+        .shadow(color: Color.clear, radius: 0, x: 0, y: 0)
+        .backgroundStyle(.clear)
+        .shadow(
+            color: Color.black.opacity(0.25),
+            radius: 3,
+            x: 0,
+            y: tabBarPosition == .top ? 1 : -1
+        )
+        .zIndex(99) // Raised so that shadow is visible above view backgrounds
+    }
+    public var body: some View {
+        
+        VStack(spacing: 0) {
+            
+            if (self.tabBarPosition == .top) {
+                tabBar
+            }
+            
+            tabViews[selection]
+                .padding(0)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            if (self.tabBarPosition == .bottom) {
+                tabBar
+            }
+        }
+        .padding(0)
     }
 }
